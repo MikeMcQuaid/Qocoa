bffd0e4d48f61b8d7ee02d4079da5407e52d544f
Leo Franchi <lfranchi@kde.org>
2011-09-05 19:06:12 +0100

    Don't include qocoa_mac.h before own header file.
    Closes #1.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- qprogressindicatorspinning_mac.mm
+++ qprogressindicatorspinning_mac.mm
@@ -20,9 +20,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "qocoa_mac.h"
-
 #include "qprogressindicatorspinning.h"
+
+#include "qocoa_mac.h"
 
 #import "Foundation/NSAutoreleasePool.h"
 #import "AppKit/NSProgressIndicator.h"
